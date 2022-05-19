fc13f7b34fcc9fb710c2932295468c0aafbf7c1a
sbelshaw <sbelshaw@users.noreply.github.com>
2022-05-19 17:57:28 +0100

    Fix compatibility issues with remote

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+
+
 def on_received_string(receivedString):
     if "forward" == receivedString:
         RingbitCar.forward()
     elif "stop" == receivedString:
         RingbitCar.brake()
+    elif "right" == receivedString:
+        RingbitCar.steering_angle(RingbitCar.Direction_turn.RIGHT, 90)
+    elif "left" == receivedString:
+        RingbitCar.steering_angle(RingbitCar.Direction_turn.LEFT, 90)
 radio.on_received_string(on_received_string)
 
 radio.set_group(68)
@@ -10,6 +16,3 @@ radio.set_group(68)
 def on_forever():
     pass
 basic.forever(on_forever)
-
-def on_received_string(receivedString):
-    if "right" == receivedString:
--- main.ts
+++ main.ts
@@ -1,12 +1,33 @@
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    if ("forward" == receivedString) {
-        RingbitCar.forward()
-    } else if ("stop" == receivedString) {
-        RingbitCar.brake()
+enum RadioMessage {
+    right = 32391,
+    left = 14947,
+    startstop = 5388,
+}
+
+let moving:boolean = false;
+
+radio.onReceivedNumber((code) => {
+    switch(code) {
+        case RadioMessage.right:
+            RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 90);
+            break;
+        case RadioMessage.left:
+            RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 90)
+            break;
+        case RadioMessage.startstop:
+            if(moving) {
+                RingbitCar.brake()
+                moving = false;
+            }else {
+                moving = true;
+            }
     }
-    
 })
-radio.setGroup(68)
-basic.forever(function on_forever() {
-    
+radio.setGroup(21)
+
+basic.forever(() => {
+    basic.showIcon(IconNames.Happy)
+    if(moving) {
+        RingbitCar.forward()
+    }
 })
